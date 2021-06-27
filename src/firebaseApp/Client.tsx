98540b46e665be623ada62951b8c35f306f59f73
Daniel Perez <daniel.perez@Daniels-MacBook-Pro.local>
2021-06-27 01:18:23 -0500

    fix: firebase deploy

--- src/firebaseApp/Client.tsx
+++ src/firebaseApp/Client.tsx
@@ -1,7 +1,14 @@
 import firebase from 'firebase/app'
 import 'firebase/firestore'
 
-const firebaseConfig = JSON.parse('{}')
+const firebaseConfig = {
+  apiKey: 'AIzaSyCSchj7fZjtIm-6E9nSLjzN3H8pnc_P7hM',
+  authDomain: 'habi-fe.firebaseapp.com',
+  projectId: 'habi-fe',
+  storageBucket: 'habi-fe.appspot.com',
+  messagingSenderId: '806269648544',
+  appId: '1:806269648544:web:48821239482dc44df913c5'
+}
 
 !firebase.apps.length && firebase.initializeApp(firebaseConfig)
 
